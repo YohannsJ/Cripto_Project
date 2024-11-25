508faae4c9aa912d9c10b52d540c279984442c09
Yohanns Jara <yohanns.jara@sansano.usm.cl>
2024-11-25 09:17:57 -0300

    Update requirements and improve temperature serializers and URLs with comments

--- Backend/temperatures/serializers.py
+++ Backend/temperatures/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Temperature
 
+# Funcion que se encarga de serializar los datos de la temperatura
 class TemperatureSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Temperature
-        fields = '__all__'
+        model = Temperature # Indicar el modelo a serializar
+        fields = '__all__' # Indicar que se serializan todos los campos
--- Backend/temperatures/urls.py
+++ Backend/temperatures/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TemperatureView
-
+# URL para acceder a las vistas de la aplicación de temperaturas
 urlpatterns = [
     path('temperatures/', TemperatureView.as_view(), name='temperature'),
 ]
